2139cbf89f048e8d56d94bbe6fb0f431201e4bf6
Arvind Menon <arvind@physera.com>
2019-12-23 14:05:49 -0600

    Updates to limit functionality for Physera's needs and address zip path vulnerability

--- android/src/main/java/com/rnziparchive/RNZipArchiveModule.java
+++ android/src/main/java/com/rnziparchive/RNZipArchiveModule.java
@@ -1,6 +1,7 @@
 package com.rnziparchive;
 
 import android.content.res.AssetFileDescriptor;
+import android.os.Build;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
@@ -22,21 +23,23 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.FileHeader;
-import net.lingala.zip4j.progress.ProgressMonitor;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+
+import java.nio.charset.Charset;
 
 public class RNZipArchiveModule extends ReactContextBaseJavaModule {
   private static final String TAG = RNZipArchiveModule.class.getSimpleName();
 
-  private static final int BUFFER_SIZE = 4096;
   private static final String PROGRESS_EVENT_NAME = "zipArchiveProgressEvent";
   private static final String EVENT_KEY_FILENAME = "filePath";
   private static final String EVENT_KEY_PROGRESS = "progress";
@@ -52,156 +55,15 @@ public class RNZipArchiveModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void isPasswordProtected(final String zipFilePath, final Promise promise) {
-    try {
-      net.lingala.zip4j.core.ZipFile zipFile = new net.lingala.zip4j.core.ZipFile(zipFilePath);
-      promise.resolve(zipFile.isEncrypted());
-    } catch (ZipException ex) {
-      promise.reject(null, String.format("Unable to check for encryption due to: %s", getStackTrace(ex)));
-    }
   }
 
   @ReactMethod
   public void unzipWithPassword(final String zipFilePath, final String destDirectory,
         final String password, final Promise promise) {
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        try {
-          net.lingala.zip4j.core.ZipFile zipFile = new net.lingala.zip4j.core.ZipFile(zipFilePath);
-          if (zipFile.isEncrypted()) {
-            zipFile.setPassword(password);
-          } else {
-            promise.reject(null, String.format("Zip file: %s is not password protected", zipFilePath));
-          }
-
-          List fileHeaderList = zipFile.getFileHeaders();
-          List extractedFileNames = new ArrayList<>();
-          int totalFiles = fileHeaderList.size();
-
-          updateProgress(0, 1, zipFilePath); // force 0%
-          for (int i = 0; i < totalFiles; i++) {
-            FileHeader fileHeader = (FileHeader) fileHeaderList.get(i);
-            zipFile.extractFile(fileHeader, destDirectory);
-            if (!fileHeader.isDirectory()) {
-              extractedFileNames.add(fileHeader.getFileName());
-            }
-            updateProgress(i + 1, totalFiles, zipFilePath);
-          }
-          promise.resolve(Arguments.fromList(extractedFileNames));
-        } catch (ZipException ex) {
-          updateProgress(0, 1, zipFilePath); // force 0%
-          promise.reject(null, String.format("Failed to unzip file, due to: %s", getStackTrace(ex)));
-        }
-      }
-    }).start();
   }
 
   @ReactMethod
-  public void unzip(final String zipFilePath, final String destDirectory, final Promise promise) {
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        // Check the file exists
-        FileInputStream inputStream = null;
-        try {
-          inputStream = new FileInputStream(zipFilePath);
-          new File(zipFilePath);
-        } catch (FileNotFoundException | NullPointerException e) {
-          if (inputStream != null) {
-            try {
-              inputStream.close();
-            } catch (IOException ignored) {
-            }
-          }
-          promise.reject(null, "Couldn't open file " + zipFilePath + ". ");
-          return;
-        }
-
-        try {
-          // Find the total uncompressed size of every file in the zip, so we can
-          // get an accurate progress measurement
-          final long totalUncompressedBytes = getUncompressedSize(zipFilePath);
-
-          File destDir = new File(destDirectory);
-          if (!destDir.exists()) {
-            //noinspection ResultOfMethodCallIgnored
-            destDir.mkdirs();
-          }
-
-          updateProgress(0, 1, zipFilePath); // force 0%
-
-          // We use arrays here so we can update values
-          // from inside the callback
-          final long[] extractedBytes = {0};
-          final int[] lastPercentage = {0};
-
-          final ZipFile zipFile = new ZipFile(zipFilePath);
-          final Enumeration<? extends ZipEntry> entries = zipFile.entries();
-          Log.d(TAG, "Zip has " + zipFile.size() + " entries");
-          while (entries.hasMoreElements()) {
-            final ZipEntry entry = entries.nextElement();
-            if (entry.isDirectory()) continue;
-
-            StreamUtil.ProgressCallback cb = new StreamUtil.ProgressCallback() {
-              @Override
-              public void onCopyProgress(long bytesRead) {
-                extractedBytes[0] += bytesRead;
-
-                int lastTime = lastPercentage[0];
-                int percentDone = (int) ((double) extractedBytes[0] * 100 / (double) totalUncompressedBytes);
-
-                // update at most once per percent.
-                if (percentDone > lastTime) {
-                  lastPercentage[0] = percentDone;
-                  updateProgress(extractedBytes[0], totalUncompressedBytes, zipFilePath);
-                }
-              }
-            };
-
-            File fout = new File(destDirectory, entry.getName());
-            String destDirCanonicalPath = (new File(destDirectory)).getCanonicalPath();
-            String canonicalPath = fout.getCanonicalPath();
-            if (!canonicalPath.startsWith(destDirCanonicalPath)) {
-              throw new Exception(String.format("Found Zip Path Traversal Vulnerability with %s", canonicalPath));
-            }
-
-            if (!fout.exists()) {
-              //noinspection ResultOfMethodCallIgnored
-              (new File(fout.getParent())).mkdirs();
-            }
-            InputStream in = null;
-            BufferedOutputStream Bout = null;
-            try {
-              in = zipFile.getInputStream(entry);
-              Bout = new BufferedOutputStream(new FileOutputStream(fout));
-              StreamUtil.copy(in, Bout, cb);
-              Bout.close();
-              in.close();
-            } catch (IOException ex) {
-              if (in != null) {
-                try {
-                  in.close();
-                } catch (Exception ignored) {
-                }
-              }
-              if (Bout != null) {
-                try {
-                  Bout.close();
-                } catch (Exception ignored) {
-                }
-              }
-            }
-          }
-
-          zipFile.close();
-          updateProgress(1, 1, zipFilePath); // force 100%
-          promise.resolve(destDirectory);
-        } catch (Exception ex) {
-          updateProgress(0, 1, zipFilePath); // force 0%
-          promise.reject(null, "Failed to extract file " + ex.getLocalizedMessage());
-        }
-      }
-    }).start();
+  public void unzip(final String zipFilePath, final String destDirectory, final String charset, final Promise promise) {
   }
 
   /**
@@ -252,11 +114,7 @@ public class RNZipArchiveModule extends ReactContextBaseJavaModule {
               if (entry.isDirectory()) continue;
               fout = new File(destDirectory, entry.getName());
 
-              String destDirCanonicalPath = (new File(destDirectory)).getCanonicalPath();
-              String canonicalPath = fout.getCanonicalPath();
-              if (!canonicalPath.startsWith(destDirCanonicalPath)) {
-                throw new Exception(String.format("Found Zip Path Traversal Vulnerability with %s", canonicalPath));
-              }
+              ensureZipPathSafety(fout, destDirectory);
 
               if (!fout.exists()) {
                 //noinspection ResultOfMethodCallIgnored
@@ -306,54 +164,11 @@ public class RNZipArchiveModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void zip(String fileOrDirectory, String destDirectory, Promise promise) {
-    List<String> filePaths = new ArrayList<>();
-
-    String fromDirectory;
-    try {
-      File tmp = new File(fileOrDirectory);
-      if (tmp.exists()) {
-        if (tmp.isDirectory()) {
-          fromDirectory = fileOrDirectory;
-          List<File> files = getSubFiles(tmp, true);
-          for (int i = 0; i < files.size(); i++) {
-            filePaths.add(files.get(i).getAbsolutePath());
-          }
-        } else {
-          fromDirectory = fileOrDirectory.substring(0, fileOrDirectory.lastIndexOf("/"));
-          filePaths.add(fileOrDirectory);
-        }
-      } else {
-        throw new FileNotFoundException(fileOrDirectory);
-      }
-    } catch (FileNotFoundException | NullPointerException e) {
-      promise.reject(null, "Couldn't open file/directory " + fileOrDirectory + ".");
-      return;
-    }
-
-    try {
-      String[] filePathArray = filePaths.toArray(new String[filePaths.size()]);
-      new ZipTask(filePathArray, destDirectory, fromDirectory, promise, this).zip();
-    } catch (Exception ex) {
-      promise.reject(null, ex.getMessage());
-      return;
-    }
   }
 
-  private List<File> getSubFiles(File baseDir, boolean isContainFolder) {
-    List<File> fileList = new ArrayList<>();
-    File[] tmpList = baseDir.listFiles();
-    for (File file : tmpList) {
-      if (file.isFile()) {
-        fileList.add(file);
-      }
-      if (file.isDirectory()) {
-        if (isContainFolder) {
-          fileList.add(file); //key code
-        }
-        fileList.addAll(getSubFiles(file, isContainFolder));
-      }
-    }
-    return fileList;
+  @ReactMethod
+  public void zipWithPassword(String fileOrDirectory, String destDirectory, String password,
+      String encryptionMethod, Promise promise) {
   }
 
   protected void updateProgress(long extractedBytes, long totalSize, String zipFilePath) {
@@ -373,10 +188,15 @@ public class RNZipArchiveModule extends ReactContextBaseJavaModule {
    *
    * @return -1 on failure
    */
-  private long getUncompressedSize(String zipFilePath) {
+  private long getUncompressedSize(String zipFilePath, String charset) {
     long totalSize = 0;
     try {
-      ZipFile zipFile = new ZipFile(zipFilePath);
+      ZipFile zipFile = null;
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        zipFile = new ZipFile(zipFilePath, Charset.forName(charset));
+      } else {
+        zipFile = new ZipFile(zipFilePath);
+      }
       Enumeration<? extends ZipEntry> entries = zipFile.entries();
       while (entries.hasMoreElements()) {
         ZipEntry entry = entries.nextElement();
@@ -400,6 +220,13 @@ public class RNZipArchiveModule extends ReactContextBaseJavaModule {
     PrintWriter pw = new PrintWriter(sw);
     e.printStackTrace(pw);
     return sw.toString();
+  }
+
+  private void ensureZipPathSafety(final File fout, final String destDirectory) throws Exception {
+    String canonicalPath = fout.getCanonicalPath();
+    if (!canonicalPath.startsWith(destDirectory)) {
+      throw new SecurityException(String.format("Found Zip Path Traversal Vulnerability with %s", canonicalPath));
+    }
   }
 
 }
